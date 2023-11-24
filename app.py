1bd1c178a4b345c1b5394acec547ec54027b5e33
Osei <williamsabodunrin@gmail.com>
2023-11-24 14:22:18 +0000

    interactive chess engine

--- app.py
+++ app.py
@@ -1,11 +1,55 @@
-from flask import Flask, render_template
+import chess
+import chess.engine
+from flask import Flask, render_template, request, jsonify, redirect
+
 
 
 app = Flask(__name__)
+engine = chess.engine.SimpleEngine.popen_uci(r"C:\Users\user\Downloads\komodo-14\komodo-14_224afb\Windows\komodo-14.1-64bit-bmi2.exe")
+
+engine_dict = {
+    'stockfish' : r"C:\Users\user\Downloads\stockfish-windows-x86-64-avx2\stockfish\stockfish-windows-x86-64-avx2.exe",
+    'komodo' : r"C:\Users\user\Downloads\komodo-14\komodo-14_224afb\Windows\komodo-14.1-64bit-bmi2.exe"
+}
+
 
 @app.route('/', strict_slashes=False)
 def index():
     return render_template('main.html')
+
+@app.route('/chess', strict_slashes=False)
+def chess_game():
+    return render_template('chess.html')
+
+@app.route('/move', methods=['POST'], strict_slashes=False)
+def move():
+    #get json file
+    data = request.get_json()
+
+    #create board instance based on player feed
+    board = chess.Board(data['fen'])
+
+    #get best result to be played based on egine
+    info = engine.analyse(board, chess.engine.Limit(time=0.1))
+    score = str(info['score'].relative)
+    board.push(info['pv'][0])
+    return jsonify({
+        'pv': str(info['pv'][0]),
+        'fen': board.fen(),
+        'score': score
+        })
+
+@app.route('/init', methods=['POST'], strict_slashes=False)
+def initiliaze():
+    print(request.form.get('comp_engine'))
+
+    #get the global engine variable
+    global engine
+
+    # set the engine to the one selected
+    engine = chess.engine.SimpleEngine.popen_uci(engine_dict[str(request.form.get('comp_engine'))])
+    print(engine.id)
+    return redirect('http://127.0.0.1:5000/chess')
 
 
 if __name__ == "__main__":
--- testing.py
+++ testing.py
@@ -1,16 +1,33 @@
 import chess
- 
-# create board object
-board=chess.Board()
-print(list(board.legal_moves))
- 
-# display chess board
-print(board.turn)
-board.push_san("e4")
-# It means moving the particular piece at 
-# e place to 4th position 
-  
-# Display chess board  
+import chess.engine
+
+engine = chess.engine.SimpleEngine.popen_uci(r"C:\Users\user\Downloads\stockfish-windows-x86-64-avx2\stockfish\stockfish-windows-x86-64-avx2.exe")
+print(dir(engine))
+print(engine.id)
+board = chess.Board()
+info = engine.analyse(board, chess.engine.Limit(time=0.1))
+print("Score:", info["score"])
+# Score: PovScore(Cp(+20), WHITE)
+
+# board = chess.Board("r1bqkbnr/p1pp1ppp/1pn5/4p3/2B1P3/5Q2/PPPP1PPP/RNB1K1NR w KQkq - 2 4")
+# info = engine.analyse(board, chess.engine.Limit(depth=20))
+# print(info)
+# print("Score:", dir(info["score"]))
+# print("Score:", str(info["score"].relative))
 print()
-print(board.turn)
-print(list(board.legal_moves))
+board.push(info['pv'][0])
+info = engine.analyse(board, chess.engine.Limit(depth=20))
+print(info)
+print("Score:", dir(info["score"]))
+print("Score:", str(info["score"].relative))
+
+print()
+board.push(info['pv'][0])
+info = engine.analyse(board, chess.engine.Limit(depth=20))
+print(info)
+print("Score:", dir(info["score"]))
+print("Score:", str(info["score"].relative))
+print("Score:", str(info["pv"][0]))
+# Score: PovScore(Mate(+1), WHITE)
+
+engine.quit()
